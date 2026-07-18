1591bd2e865c3d26d6f89c43a8f5e09fe9c2d935
megpara <meganparadowski@gmail.com>
2026-07-18 16:58:43 -0700

    Shrink poster image and add priority to biopic

--- pages/about.js
+++ pages/about.js
@@ -18,7 +18,7 @@ export default function About() {
                         <div className="md:basis-1/2 text-3xl font-display font-bold">Hi, we're Colin & Meg — a husband and wife director duo based in LA. Together we direct, DP, edit, light, cast, and choreograph.</div>
                     </div>
                     <div className="block md:flex justify-between pt-12 md:pt-[24vh] pb-[12vh] px-4 md:px-8">
-                        <Image src="/biopic.JPG" alt="Colin Lupe and Megan Paradowski" className="basis-1/2 max-w-full md:max-w-[400px] w-full object-cover pb-12" width={400} height={300}/>
+                        <Image src="/biopic.JPG" alt="Colin Lupe and Megan Paradowski" className="basis-1/2 max-w-full md:max-w-[400px] w-full object-cover pb-12" width={400} height={300} priority />
                         <div className="basis-1/2 text-[#bab5b1] text-sm text-justify italic">
                             <strong className="font-bold">Colin Lupe</strong> is a creator, director, DP, cinematographer, and editor based in Los Angeles. Colin is best known for his work on Thank You, Goodnight: The Bon Jovi Story (2024), BS High (2023), and Two Distant Strangers (2020). He is honored to have received Emmy and Oscar awards for his work on Olympic campaign #StrongerTogether and Two Distant Strangers, respectively. Colin has worked with brands and artists such as Bon Jovi, Lizzo, Nike, Olipop, Meta, Naked Cashmere, and others.
                             <br />
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <Layout>
             <div className="w-screen h-screen relative">
-            <video loop autoPlay muted playsInline className="absolute w-screen h-screen top-0 left-0 object-cover" src="/work/directorreel.mp4" poster="/work/bluemoon.png" />
+            <video loop autoPlay muted playsInline className="absolute w-screen h-screen top-0 left-0 object-cover" src="/work/directorreel.mp4" poster="/work/bluemoon.jpg" />
             <div className="absolute bottom-16 w-full h-[12vh] flex items-end text-[8px] md:text-[10px] tracking-widest px-8">
                 <div className="basis-1/3">
                     <div className="max-w-[75px] md:max-w-[150px] uppercase">
