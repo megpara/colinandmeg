55351d069b1f684dd33aecbaaaeb2952bd3a14c6
megpara <meganparadowski@gmail.com>
2026-07-18 17:07:33 -0700

    Update mobile sizing of index subtext

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
         <Layout>
             <div className="w-screen h-screen relative">
             <video loop autoPlay muted playsInline className="absolute w-screen h-screen top-0 left-0 object-cover" src="/work/directorreel.mp4" poster="/work/bluemoon.jpg" />
-            <div className="absolute bottom-16 w-full h-[12vh] flex items-end text-[8px] md:text-[10px] tracking-widest px-8">
+            <div className="absolute bottom-32 md:bottom-16 w-full h-[12vh] flex items-end text-[8px] md:text-[10px] tracking-widest px-4 md:px-8">
                 <div className="basis-1/3">
                     <div className="max-w-[75px] md:max-w-[150px] uppercase">
                     LA-Based director duo
